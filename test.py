99417449c9c1c3b6b2d382e28af90f80b719a994
faraway <fffffaraway@gmail.com>
2022-03-31 16:28:01 +0800

    delete unused args

--- test.py
+++ test.py
@@ -10,16 +10,6 @@ args = {
     'h_dim': 128,
     'dropout': 0.1,
     'random_seed': 2021,
-    'train_r': 0.8,
-    'test_r': 0.2,
-    'val_r': 0.1,
-    'batch_size': 4,
-    'lr': 3e-06,
-    'epochs': 200,
-    'save': False,
-    'dif_coef': 1,
-    'sim_coef': 1,
-    'recon_coef': 1,
     'split_by': 'clip',  # clip or speaker
     'device': 'cpu',  # cpu or cuda
 }
